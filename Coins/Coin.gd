b599fe07dc61b0db12866ba21c87528d5801deb7
oleshka275 <cat.dog.3415@gmail.com>
2020-12-10 22:36:34 +0300

    release v2.3

--- Coins/Coin.gd
+++ Coins/Coin.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var get_coin = false
 
+onready var animation = $AnimationPlayer
+
+func _ready():
+	animation.play("Coins")
+
 func _on_Hurtbox_area_entered(area):
 	get_coin = true
 	
